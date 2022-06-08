9884142eb0917c1c300a1c624600566c5c67dadc
Jonathan Compton <jonathankcompton@gmail.com>
2022-06-07 21:49:57 -0700

    add config for deploy

--- db.js
+++ db.js
@@ -7,9 +7,23 @@ const URI =
     : process.env.DB_URI;
 
 const db = new Client({
-  connectionString: process.env.DATABASE_URL || `postgresql:///${URI}`
+  connectionString: process.env.DATABASE_URL || `postgresql:///${URI}`,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect();
+
+db.query(
+  "SELECT table_schema, table_name FROM information_schema.tables;",
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    db.end();
+  }
+);
 
 module.exports = db;
